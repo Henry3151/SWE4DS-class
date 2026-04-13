1d07144570ee5f5e93e01714344aa21695d61aaa
Henrique Silva <henry_s09@hotmail.com>
2026-04-13 17:33:04 -0300

    feat: atualiza URL para endpoint csv

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,8 +2,9 @@
 import pandas as pd
 
 # URL do dataset
-DATASET_URL = "https://exemplo.com/dados.xls"
+DATASET_URL = "https://exemplo.com/dados.csv"
 
 def carregar_dados():
+    DATASET_URL = "https://exemplo.com/dados.zip"
     df = pd.read_csv(DATASET_URL)
     return df
